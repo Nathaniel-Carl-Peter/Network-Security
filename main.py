29197e6eeee20b80f2ef9ec619de25869cb1d067
Lucas <lucas.obrien@my.jcu.edu.au>
2024-06-12 19:30:32 +1000

    Complete task 5

--- main.py
+++ main.py
@@ -22,44 +22,47 @@ import hashlib
 
 VALID_CHARACTERS = ('a b c d e f g h i j k l m n o p q r s t u v w x y z '
                     'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z')
-
 SPECIAL_CHARACTERS = "! @ # $ % ^ & * ( ) _ - = + ` ~ , . / ' [ ] < > ? { } | : ;"
-
 REQUIREMENTS = "Password must have a lower case, upper case letter, 1 number and a special character"
-
-# length_of_password = random.randint(8, 20)
 MINIMUM_LENGTH_OF_PASSWORD = 8
 MAXIMUM_LENGTH_OF_PASSWORD = 20
+MENU_OPTIONS = "Menu\n(G)enerate new user and password\n(C)heck user and password\n(Q)uit\n"
 
 
 def main():
-    # valid_characters = VALID_CHARACTERS.split()
-    # special_characters = SPECIAL_CHARACTERS.split()
+    # import user records
+    records = import_details()
+    menu_choice = input(MENU_OPTIONS).lower()
+
+    while menu_choice != 'q':
+        if menu_choice == 'g':
+            add_user(records)
+        elif menu_choice == 'c':
+            check_username = input("Enter username: ")
+            check_password = input("Enter password: ")
+            validate_password(records, check_username, check_password)
+        # the following is only for when i break the code
+        # elif menu_choice == 'f':
+        #     salt = salt_password()
+        #     hashed_password = hash_password("abCD##33", salt)
+        #     fix_password("Lucas", hashed_password, salt)
+        menu_choice = input(MENU_OPTIONS).lower()
+
+    export_password(records)
+
+
+def add_user(records):
+    """Add a user to the current records."""
     username = input("input username: ")
     print(REQUIREMENTS)
     valid_password = input("< ")
     while not is_valid_password(valid_password):
-        # password = []
-        #
-        # count += 1
-        # for i in range(length_of_password):
-        #
-        #     character_choice = random.randint(0, 2)
-        #     if character_choice == 0:
-        #         password.append(valid_characters[random.randint(0, len(valid_characters) - 1)])
-        #     elif character_choice == 1:
-        #         password.append(special_characters[random.randint(0, len(special_characters) - 1)])
-        #     else:
-        #         password.append(str(random.randint(0, 9)))
-        #     print(password)
-        # valid_password = ''.join(password)
         print('Invalid Password!\n', REQUIREMENTS)
         valid_password = input("Please enter a password: ")
-    print(valid_password)
-
-    valid_password = hash_password(valid_password, salt_password())
-    print(valid_password)
-    export_password(username, valid_password)
+    salt = salt_password()
+    valid_password = hash_password(valid_password, salt)
+    new_user = {"username": username, "password": valid_password, "salt": salt}
+    records.append(new_user)
 
 
 def is_valid_password(password):
@@ -91,24 +94,54 @@ def is_valid_password(password):
 
 
 def salt_password():
+    """Salt the password with a randomly generated key."""
     salt = ''.join([chr(int(random.uniform(33, 126))) for _ in range(16)])
     return salt
 
 
 def hash_password(password, salt):
+    """Hash the password with the generated salt."""
     salted_password = salt + password
     hash_object = hashlib.sha256(salted_password.encode())
     hashed_password = hash_object.hexdigest()
     return hashed_password
 
 
-def export_password(username, password):
-    # with open("password.txt", "w") as out_file:
-    #     print(password, file=out_file)
+def export_password(records):
+    """Save all records to json file."""
     save_details = []
     with open("password.json", "w", encoding="UTF-8") as out_file:
-        key_to_details = {"username": username, "password": password}
+        for record in records:
+            key_to_details = {"username": record['username'], "password": record['password'], "salt": record['salt']}
+            save_details.append(key_to_details)
+        json.dump(save_details, out_file)
+
+
+def validate_password(records, username, password):
+    """Checks if a username is valid, and if the entered password matches the user record."""
+    for record in records:
+        if record['username'] == username:
+            test_hash = hash_password(password, record['salt'])
+            if record['password'] == test_hash:
+                print("Success!")
+            else:
+                print("Failure!")
+
+
+def import_details():
+    """Load in records from a json file."""
+    with open("password.json", "r", encoding="UTF-8") as in_file:
+        records = json.load(in_file)
+    return records
+
+
+def fix_password(username, password, salt):
+    """ONLY FOR WHEN I BREAK THE JSON FILE"""
+    save_details = []
+    with open("password.json", "w", encoding="UTF-8") as out_file:
+        key_to_details = {"username": username, "password": password, "salt": salt}
         save_details.append(key_to_details)
         json.dump(save_details, out_file)
+
 
 main()
